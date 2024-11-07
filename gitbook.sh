3d76a57d7fc30881e73648e17e0b472cc6129b7c
qierkang <qierkang@leoao.com>
2024-11-08 01:01:47 +0800

    refactor: 移除未使用的变量 - 优化代码结构，提高代码可读性和性能

--- gitbook.sh
+++ gitbook.sh
@@ -3,7 +3,7 @@
 # 函数：执行 Git 操作并显示消息
 git_operation() {
     echo "$1"
-    sleep 10  # 增加睡眠时间以便观察
+    sleep 1  # 增加睡眠时间以便观察
     git $2
 }
 
@@ -22,21 +22,21 @@ git_operation "🎉 开始更新仓库 git pull" pull
 # 检查是否有文件被修改
 if [ -z "$(git status --porcelain)" ]; then
     echo "没有文件被修改，无需提交。"
-    exit 8
+    exit 2
 fi
 
 # 添加本地文件
-git_operation "🐧 添加本地文件 git add ." add .
+#git_operation "🐧 添加本地文件 git add ." add .
 
 # 再次检查是否有文件被添加到暂存区
-if [ -z "$(git diff --cached --name-only)" ]; then
-    echo "错误：没有文件被添加到暂存区。"
-    exit 9
-fi
+#if [ -z "$(git diff --cached --name-only)" ]; then
+#    echo "错误：没有文件被添加到暂存区。"
+#    exit 2
+#fi
 
 # 提交更新
-# git_operation "💡 提交更新 git commit -m '$1'" commit -m "'$1'"
+git_operation "💡 提交更新 git commit -m '$1'" commit -m "'$1'"
 
 # 推送仓库
 # 如果你想要推送，可以取消下面这一行的注释
-# git_operation "🖥️ 推送仓库 git push -u origin master" push -u origin master
+git_operation "🖥️ 推送仓库 git push -u origin master" git push -f git@github.com:pms-pro/pms-doc.git master
