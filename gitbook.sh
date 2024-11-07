f97df92673875fd81b43281f69025b283faf74c3
qierkang <qierkang@leoao.com>
2024-11-08 01:06:00 +0800

    refactor: 移除未使用的变量 - 优化代码结构，提高代码可读性和性能

--- gitbook.sh
+++ gitbook.sh
@@ -7,9 +7,6 @@ git_operation() {
     git $2
 }
 
-# 输出调试信息
-echo "提交信息: $1"
-
 # 检查当前目录是否是 Git 仓库
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
     echo "错误：当前目录不是一个 Git 仓库。"
@@ -19,19 +16,7 @@ fi
 # 更新本地代码
 git_operation "🎉 开始更新仓库 git pull" pull
 
-# 添加本地文件
-#git_operation "🐧 添加本地文件 git add ." add .
-
-# 再次检查是否有文件被添加到暂存区
-#if [ -z "$(git diff --cached --name-only)" ]; then
-#    echo "错误：没有文件被添加到暂存区。"
-#    exit 2
-#fi
-
-# 提交更新
-#git_operation "💡 提交更新 git commit -m '$1'" commit -m "'$1'"
-
 # 推送仓库
-# 如果你想要推送，可以取消下面这一行的注释
-#git_operation "🖥️ 推送仓库 git push -u origin master" push -f git@github.com:pms-pro/pms-doc.git master
+echo "🖥️ 推送仓库 git push -u origin master"
+sleep 1  # 增加睡眠时间以便观察
 git push -f git@github.com:pms-pro/pms-doc.git master
